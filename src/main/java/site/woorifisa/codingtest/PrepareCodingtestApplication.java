1a18fbb817fb90c6cbc1cfaedbb091685bd42777
namsh1125 <namsh1125@naver.com>
2025-02-03 20:39:51 +0900

    Init: 프로젝트 초기 설정

---
+++ src/main/java/site/woorifisa/codingtest/PrepareCodingtestApplication.java
@@ -0,0 +1,13 @@
+package site.woorifisa.codingtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrepareCodingtestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrepareCodingtestApplication.class, args);
+    }
+
+}
